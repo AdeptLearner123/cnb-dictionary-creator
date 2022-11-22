53b2f5bd632105d74ef26b7ab59089f6e6cd2ca3
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-11-22 15:16:19 -0800

    Log for logarithm

--- cnb_oxford_dictionary/clue_filter/clue_tokens_extractor.py
+++ cnb_oxford_dictionary/clue_filter/clue_tokens_extractor.py
@@ -9,7 +9,7 @@ def get_clue_variants(json):
     if "variantForms" not in json:
         return []
     
-    variants = [ variant_form["text"] for variant_form in json["variantForms"] ]
+    variants = [ variant_form["text"].upper() for variant_form in json["variantForms"] ]
     variants = [ variant for variant in variants if len(tokenize(variant)) == 1 ]
     return variants
 
@@ -21,13 +21,16 @@ def extract_clue_tokens(lexical_entry, entry, sense):
     if lexical_category not in LEXICAL_CATEGORIES:
         return None
     
+    clue_tokens = []
     tokens = tokenize(word)
     if len(tokens) == 1:
-        return tokens
+        clue_tokens += tokens
 
     clue_variants = get_clue_variants(entry) + get_clue_variants(sense)
-    if len(clue_variants) > 0:
-        return clue_variants
+    clue_tokens += clue_variants
+
+    if len(clue_tokens) > 0:
+        return clue_tokens
 
     if is_proper(entry):
         if ", " in word:
